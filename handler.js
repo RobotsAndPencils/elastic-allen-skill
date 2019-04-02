9db36f4ec54e37f050c0d8600bd8b385051d86e7
Joshua Page <josh.page@robotsandpencils.com>
2019-04-02 13:47:21 -0400

    successfull end to end with elastic Allen

--- handler.js
+++ handler.js
@@ -6,11 +6,21 @@ const request = require('request-promise');
 //const fs = require('fs');
 //const boxSdkConfig = JSON.parse(fs.readFileSync('./conf/boxSdkConfig.json', 'utf8'));
 
+const mimeMapping = {
+  txt: 'text/plain',
+  docx: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+  pdf: 'application/pdf'
+}
+const getMimeType = (extension) => {
+  const mime = mimeMapping[extension];
+  if (!mime) mime=extension;
+  return mime;
+}
 
-const sendUrlToEA = async (fileURL, viewURL) => {
+const sendUrlToEA = async (fileURL, viewURL, contentType) => {
   console.log('making request');
   try {
-    const response = await request({
+    const options = {
       method: 'POST',
       uri: 'http://eabot.us-west-1.elasticbeanstalk.com/demoimport',
       body: {
@@ -18,10 +28,13 @@ const sendUrlToEA = async (fileURL, viewURL) => {
         token: 'aaede76cd9e447371fe8fea83ea56445', 
         url: fileURL,
         viewURL: viewURL,
+        contentType: contentType,
       },
       json: true,
-      timeout: 10000,
-    });
+      timeout: 10000,    
+    };
+    console.log('request options: ', options);
+    const response = await request(options);
     console.log('Response: ', response);
   } catch (e) {
     console.log('Exception thrown: ', e);
@@ -32,17 +45,14 @@ module.exports.indexContent = async (event, context) => {
   console.log('Lambda Execution starting');
   const reader = new FilesReader(event.body);
   const fileURL = reader.fileDownloadURL;
-
+  const contentType = getMimeType(reader.fileFormat);
   // const client = reader.fileWriteClient;  
   // console.log(client);
   // const file = await client.files.update(reader.getFileContext().fileId, {shared_link: client.accessLevels.DEFAULT});
   // const shareURL = file.shared_link.url;
   // TODO: currently no way to make a jwt client for box skill app
   const shareURL = 'https://robotsandpencils.app.box.com/folder/66732684150';
-  
-  console.log(`readOnlyURL: ${fileURL}`);
-  console.log(`viewURL: ${shareURL}`);
-  await sendUrlToEA(fileURL, shareURL);
+  await sendUrlToEA(fileURL, shareURL, contentType);
   return {
     statusCode: 200,
     body: JSON.stringify({
